18a9b8faf83cb410d3d9ec619f19f371670e4f32
Felipe Baccin <felipebaccincvel@gmail.com>
2015-11-12 10:13:57 -0200

    Criação entidade produto

--- SistemaCompletoReoferta/src/br/univel/entity/Produto.java
+++ SistemaCompletoReoferta/src/br/univel/entity/Produto.java
@@ -1,5 +1,64 @@
 package br.univel.entity;
 
+import java.math.BigDecimal;
+
 public class Produto {
+
+	private int id; 
+	private int codigoBarras;
+	private String categoria;
+	private String descricao;
+	private int unidade;
+	private BigDecimal custo;
+	private BigDecimal margemLucro;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(int codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getUnidade() {
+		return unidade;
+	}
+	public void setUnidade(int unidade) {
+		this.unidade = unidade;
+	}
+	public BigDecimal getCusto() {
+		return custo;
+	}
+	public void setCusto(BigDecimal custo) {
+		this.custo = custo;
+	}
+	public BigDecimal getMargemLucro() {
+		return margemLucro;
+	}
+	public void setMargemLucro(BigDecimal margemLucro) {
+		this.margemLucro = margemLucro;
+	}
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", codigoBarras=" + codigoBarras
+				+ ", categoria=" + categoria + ", descricao=" + descricao
+				+ ", unidade=" + unidade + ", custo=" + custo
+				+ ", margemLucro=" + margemLucro + "]";
+	}
 
 }

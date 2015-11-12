3083d45a619bdc733a7e468cb00cf213d61c7b17
Felipe Baccin <felipebaccincvel@gmail.com>
2015-11-12 09:52:38 -0200

    Criação da entidade usuario

--- SistemaCompletoReoferta/src/br/univel/entity/Usuario.java
+++ SistemaCompletoReoferta/src/br/univel/entity/Usuario.java
@@ -1,5 +1,35 @@
 package br.univel.entity;
 
 public class Usuario {
-
+	
+	private int id;
+	private Cliente clientId;
+	private String senha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Cliente getClientId() {
+		return clientId;
+	}
+	public void setClientId(Cliente clientId) {
+		this.clientId = clientId;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", Clientid=" + clientId + ", senha="
+				+ senha + "]";
+	}
+	
+	
+	
 }
